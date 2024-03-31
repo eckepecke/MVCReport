b6160b075aac793e7671d22e4b09b18425866a8b
eckepecke <erik_olofsson@icloud.com>
2024-03-31 19:39:10 +0200

    Did some styling

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 import hello from './js/hello.js';
